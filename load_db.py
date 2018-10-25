fd67b059dee5da4582fcaab1a52a74841e605e35
Matt <matthias.heerens@gmail.com>
2018-10-25 09:47:35 +0200

    initialized load_db.py

---
+++ load_db.py
@@ -0,0 +1,5 @@
+"""Load all data from the Spreadsheet into a database
+
+- write a separate script for loading the data
+- use sqlite3, see: https://github.com/krother/Python3_Package_Examples/
+- try loading the data into a DataFrame """
