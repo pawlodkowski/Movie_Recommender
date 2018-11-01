36fae231d987d148c5430f26ee10e8b09cf3a5dd
Paul Wlodkowski <PaulWlodkowski@Pauls-MacBook-Pro-2.local>
2018-11-01 17:00:08 +0100

    added function to convert outputted movie IDs to titles (for testing)

--- core.py
+++ core.py
@@ -40,34 +40,34 @@ def create_users_vs_movies_matrix(df):
 def load_NMF_model(NMF_model_directory):
     loaded_model = pickle.load(open(NMF_model_directory, "rb"))
     return loaded_model
-        
+
 
 def apply_NMF(trained_model, converted_user_input, filtered_movie_ids, all_movie_ids):
-    
+
     # preparing P and Q
     P = trained_model.components_
     user_Q = trained_model.transform([converted_user_input])
-    
+
     # creating new R, = list of NMF_scores for particular user
     user_R = np.dot(user_Q, P)
     NMF_scores_user = list(user_R[0])
-    
+
     # labelling NMF_scores with movie ids
     recommendations_with_movie_id = dict(zip(all_movie_ids, NMF_scores_user))
-    
+
     # applying filters
     recommendations_with_movie_id_filtered = list([[x,recommendations_with_movie_id[x]] for x in filtered_movie_ids])
     results = pd.DataFrame(recommendations_with_movie_id_filtered, columns = ["movieId", "NMF_score"])
-    
+
     results = results.sort_values(by = "NMF_score", ascending = False)
     output = list(results["movieId"])
     return output
 
 
 def apply_CF(converted_user_input, all_movie_ids, users_vs_movies_matrix, filtered_movie_ids):
-    
+
     def make_cosine_heatmap(df):
-        labels = list(df.index.values) 
+        labels = list(df.index.values)
         cosine_similarities = cosine_similarity(df)
         results_df = pd.DataFrame(cosine_similarities, columns = labels, index = labels)
         return results_df
@@ -76,21 +76,21 @@ def apply_CF(converted_user_input, all_movie_ids, users_vs_movies_matrix, filter
         results_for_one_user = cosine_heatmap.loc[userId].sort_values(ascending = False) # get similar users
         similar_users = list(results_for_one_user.keys())[1:]
         return similar_users
-    
+
     #create user row in dataframe
     #user_input_df_row = pd.DataFrame(converted_user_input, index = all_movie_ids).transpose()
-    
+
     # add that user row into existing df
     users_vs_movies_matrix_complemented = users_vs_movies_matrix.copy()
     users_vs_movies_matrix_complemented.loc[0] = converted_user_input#user_input_df_row
-    
+
     # determine ordered list of similar users via cosine similarity
     cosine_heatmap = make_cosine_heatmap(users_vs_movies_matrix_complemented)
     similar_users = get_similar_users(cosine_heatmap, 0)
-    
+
     # apply filters
     filtered_uvmm_complemented =  users_vs_movies_matrix_complemented["rating"][filtered_movie_ids]
-    
+
     # go through every similar user, starting from most similar one, check for conditions below and append movieId to list
     recommended_movie_ids = []
     for user in similar_users:
@@ -98,7 +98,7 @@ def apply_CF(converted_user_input, all_movie_ids, users_vs_movies_matrix, filter
         d = dict(checking_movies)
         recommended_movie_ids_from_user = list(filter(d.get, d)) #returning keys (movieids) from dict where value is True
         recommended_movie_ids += recommended_movie_ids_from_user
-        recommended_movie_ids = list(unique_everseen(recommended_movie_ids)) # delete duplicates from list keeping their order 
+        recommended_movie_ids = list(unique_everseen(recommended_movie_ids)) # delete duplicates from list keeping their order
         if len(recommended_movie_ids) >= 30:
             break
     return recommended_movie_ids
@@ -142,11 +142,26 @@ def convert_django(dataframe, django_data, database_directory):
 
     return list(combined.loc[0])
 
+def convert_ids_to_titles(id_list):
+
+    query = "SELECT movieId, title FROM movielens"
+    df_translator = pd.read_sql(query, db)
+    movie_IDs = list(df_translator['movieId'])
+    titles = list(df_translator['title'])
+
+    id_2_title = dict(zip(movie_IDs, titles))
+
+    titles = []
+    for i in id_list:
+        titles.append(id_2_title[i])
+
+    return titles
+
 
 #def make_final_recommendations(recoms_NMF, recoms_CF):
     # create function that merges the results of NMF and collaborative filtering
 #    return final_recommendations
-    
+
 
 #def convert_to_imdbid(movieids):
     # converts list of movieids into list of imdb ids
@@ -156,36 +171,36 @@ def convert_django(dataframe, django_data, database_directory):
 #def make_poster_links(imdbids):
     # return a list of posterlinks according to the imdbids
 #    return poster_links
-    
+
 
 
 ############################################################################### main function
 
 def recommender(website_user_ratings, website_filters):
-    
+
     database_directory = "data/movies.db"
     NMF_model_directory = "data/NMF_model_trained.sav"
-    
-    
+
+
     d0 = get_data_from_db(database_directory, "ratings")
     users_vs_movies_matrix = create_users_vs_movies_matrix(d0)
-    
+
     converted_user_input = convert_django(users_vs_movies_matrix, website_user_ratings, database_directory)
     #filtered_movie_ids = convert_filters(website_filters)
 
     all_movie_ids = get_all_movie_ids(database_directory, tablename = "ratings")
     trained_model = load_NMF_model(NMF_model_directory)
-    
+
     filtered_movie_ids = all_movie_ids # for now
-    
+
     NMF_results = apply_NMF(trained_model, converted_user_input, filtered_movie_ids, all_movie_ids)
     CF_results = apply_CF(converted_user_input, all_movie_ids, users_vs_movies_matrix, filtered_movie_ids)
-    
+
     #final_recommendations = make_final_recommendations(NMF_results, CF_results)
-    
+
     # map to imbdid
     # make poster links
-    
+
     return len(NMF_results) , len(CF_results)
     #return len(converted_user_input)
 
