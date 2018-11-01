aaa252d35867a174fea9e695f3456eaa9a6ee802
Matt <matthias.heerens@gmail.com>
2018-11-01 13:12:22 +0100

    new script for creating NMF model

---
+++ train_NMF_model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sqlite3
+from sklearn.decomposition import NMF
+import pickle
+
+database_directory = "data/movies.db"
+tablename = "ratings"
+n_components = 10
+filename = "data/NMF_model_trained.sav"
+
+
+def get_data_from_db(database_directory, tablename):
+    db = sqlite3.connect(database_directory)
+    query = f"SELECT * FROM {tablename}"
+    df = pd.read_sql(query, db)
+    db.close()
+    return df
+
+def create_users_vs_movies_matrix(df):
+    df = df.drop("timestamp", axis = 1)
+    df = df.set_index(["userId", "movieId"])
+    df = df.unstack()
+    df = df.fillna(0)
+    return df
+
+d0 = get_data_from_db(database_directory, tablename)
+R = create_users_vs_movies_matrix(d0)
+model = NMF(n_components=n_components, init='random', random_state=42)
+model.fit(R)
+
+pickle.dump(model, open(filename, 'wb'))
